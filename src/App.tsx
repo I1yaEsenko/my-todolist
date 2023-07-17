ef3b3d32a57e6ff9347b7558f6a4a6f1f0f666f0
Ilya Esenko <ilya.esenko@gmail.com>
2023-07-17 19:23:49 +0300

    update styles and add reducer for test

--- src/App.tsx
+++ src/App.tsx
@@ -4,212 +4,189 @@ import {Todolist} from "./todolist";
 import {v1} from "uuid";
 import t from "./App.module.css";
 import {AddItemForm} from "./components/AddItemForm";
-import {AppBar, Button, IconButton, Menu, Toolbar, Typography} from "@mui/material";
-
+import {AppBar, Button, Container, Grid, Menu, Paper, Toolbar, Typography} from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Header from "./components/Header";
 
 export type TasksType = {
-  id: string
-  title: string
-  isDone: boolean
+   id: string
+   title: string
+   isDone: boolean
 }
 
 export type FilterType = 'All' | 'Active' | 'Completed'
 
 export type TodolistType = {
-  id: string
-  title: string
-  filter: FilterType
+   id: string
+   title: string
+   filter: FilterType
 }
 
 export type TaskStateType = {
-  [key: string]: Array<TasksType>
+   [key: string]: Array<TasksType>
 }
 
 function App() {
 
 
-  //local state for our data
-  // let [tasks, setTasks] = useState<Array<TasksType>>([
-  //         {id: v1(), title: "HTML&CSS", isDone: true},
-  //         {id: v1(), title: "JS", isDone: true},
-  //         {id: v1(), title: "ReactJS", isDone: false},
-  //         {id: v1(), title: "Hello world", isDone: true},
-  //         {id: v1(), title: "I am Happy", isDone: false},
-  //         {id: v1(), title: "Yo", isDone: false},
-  //     ]
-  // )
+   //local state for our data
+   // let [tasks, setTasks] = useState<Array<TasksType>>([
+   //         {id: v1(), title: "HTML&CSS", isDone: true},
+   //         {id: v1(), title: "JS", isDone: true},
+   //         {id: v1(), title: "ReactJS", isDone: false},
+   //         {id: v1(), title: "Hello world", isDone: true},
+   //         {id: v1(), title: "I am Happy", isDone: false},
+   //         {id: v1(), title: "Yo", isDone: false},
+   //     ]
+   // )
 
-  let todolistId1 = v1()
-  let todolistId2 = v1()
+   let todolistId1 = v1()
+   let todolistId2 = v1()
 
-  let [todolist, setTodolist] = useState<Array<TodolistType>>([
-    {id: todolistId1, title: 'What to learn', filter: 'All'},
-    {id: todolistId2, title: 'What to do', filter: 'All'},
-  ])
+   let [todolist, setTodolist] = useState<Array<TodolistType>>([
+      {id: todolistId1, title: 'What to learn', filter: 'All'},
+      {id: todolistId2, title: 'What to do', filter: 'All'},
+   ])
 
-  let [tasks, setTasks] = useState<TaskStateType>({
-      [todolistId1]: [
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JS", isDone: true},
-        {id: v1(), title: "ReactJS", isDone: false},
-        {id: v1(), title: "Hello world", isDone: true},
-        {id: v1(), title: "I am Happy", isDone: false},
-        {id: v1(), title: "Yo", isDone: false},
-      ],
-      [todolistId2]: [
-        {id: v1(), title: "Homework", isDone: true},
-        {id: v1(), title: "Codewars", isDone: false},
-        {id: v1(), title: "Do exams", isDone: false},
-        {id: v1(), title: "Running", isDone: true},
-        {id: v1(), title: "Working", isDone: false},
-      ],
-    }
-  )
+   let [tasks, setTasks] = useState<TaskStateType>({
+        [todolistId1]: [
+           {id: v1(), title: "HTML&CSS", isDone: true},
+           {id: v1(), title: "JS", isDone: true},
+           {id: v1(), title: "ReactJS", isDone: false},
+           {id: v1(), title: "Hello world", isDone: true},
+           {id: v1(), title: "I am Happy", isDone: false},
+           {id: v1(), title: "Yo", isDone: false},
+        ],
+        [todolistId2]: [
+           {id: v1(), title: "Homework", isDone: true},
+           {id: v1(), title: "Codewars", isDone: false},
+           {id: v1(), title: "Do exams", isDone: false},
+           {id: v1(), title: "Running", isDone: true},
+           {id: v1(), title: "Working", isDone: false},
+        ],
+     }
+   )
 
-  //local state for filter
-  // let [filter, setFilter] = useState<FilterType>('All')
+   //local state for filter
+   // let [filter, setFilter] = useState<FilterType>('All')
 
-  // let tasksForTodolist = tasks
-  // let filterTasks = () => {
-  //     tasksForTodolist = filter === 'Active' ? tasks.filter(t => t.isDone) :
-  //         filter === 'Completed' ? tasks.filter(t => !t.isDone) :
-  //             tasksForTodolist
-  // }
+   // let tasksForTodolist = tasks
+   // let filterTasks = () => {
+   //     tasksForTodolist = filter === 'Active' ? tasks.filter(t => t.isDone) :
+   //         filter === 'Completed' ? tasks.filter(t => !t.isDone) :
+   //             tasksForTodolist
+   // }
 
-  // фильтрация тасок
-  // if (filter === 'Active') {
-  //     tasksForTodolist = tasks.filter(t => !t.isDone)
-  // }
-  //
-  // if (filter === 'Completed') {
-  //     tasksForTodolist = tasks.filter(t => t.isDone)
-  // }
-  //Работа с тудулистами
-  const addTodolist = (title: string) => {
-    let newTodolistId = v1()
-    setTodolist([{id: newTodolistId, title: title, filter: 'All'}, ...todolist])
-    setTasks({...tasks, [newTodolistId]: []})
-  }
-  const todolistTitleChange = (todolistId: string, title: string) => {
-    setTodolist(todolist.map(f => f.id === todolistId ? {...f, title: title} : f))
-  }
+   // фильтрация тасок
+   // if (filter === 'Active') {
+   //     tasksForTodolist = tasks.filter(t => !t.isDone)
+   // }
+   //
+   // if (filter === 'Completed') {
+   //     tasksForTodolist = tasks.filter(t => t.isDone)
+   // }
+   //Работа с тудулистами
+   const addTodolist = (title: string) => {
+      let newTodolistId = v1()
+      setTodolist([{id: newTodolistId, title: title, filter: 'All'}, ...todolist])
+      setTasks({...tasks, [newTodolistId]: []})
+   }
+   const todolistTitleChange = (todolistId: string, title: string) => {
+      setTodolist(todolist.map(f => f.id === todolistId ? {...f, title: title} : f))
+   }
 
-  const deleteTodolist = (todolistId: string) => {
-    setTodolist(todolist.filter(filteredTodolist => filteredTodolist.id !== todolistId))
-    delete tasks[todolistId]
-    setTasks({...tasks})
-  }
-  //работа с тасками
+   const deleteTodolist = (todolistId: string) => {
+      setTodolist(todolist.filter(filteredTodolist => filteredTodolist.id !== todolistId))
+      delete tasks[todolistId]
+      setTasks({...tasks})
+   }
+   //работа с тасками
 
-  const changeFilter = (todolistId: string, value: FilterType) => {
-    // setFilter(value)
-    setTodolist(todolist.map
-    (filtered => filtered.id === todolistId ? {...filtered, filter: value} : filtered))
-  }
-  const addTask = (todolistId: string, newTitle: string) => {
-    // let newTask = [{id: v1(), title: newTitle, isDone: false}, ...tasks]
-    // setTasks(newTask)
-    let newTask = {id: v1(), title: newTitle, isDone: false}
-    setTasks({...tasks, [todolistId]: [newTask, ...tasks[todolistId]]})
-  }
-  const deleteTask = (todolistId: string, taskId: string) => {
-    // setTasks(tasks.filter(t => t.id !== idTask))
-    setTasks({...tasks, [todolistId]: tasks[todolistId].filter(d => d.id !== taskId)})
+   const changeFilter = (todolistId: string, value: FilterType) => {
+      // setFilter(value)
+      setTodolist(todolist.map
+      (filtered => filtered.id === todolistId ? {...filtered, filter: value} : filtered))
+   }
+   const addTask = (todolistId: string, newTitle: string) => {
+      // let newTask = [{id: v1(), title: newTitle, isDone: false}, ...tasks]
+      // setTasks(newTask)
+      let newTask = {id: v1(), title: newTitle, isDone: false}
+      setTasks({...tasks, [todolistId]: [newTask, ...tasks[todolistId]]})
+   }
+   const deleteTask = (todolistId: string, taskId: string) => {
+      // setTasks(tasks.filter(t => t.id !== idTask))
+      setTasks({...tasks, [todolistId]: tasks[todolistId].filter(d => d.id !== taskId)})
 
-  }
-  const taskChecked = (todolistId: string, idTask: string, isDone: boolean) => {
-    // setTasks(tasks.map( t => t.id === idTask ? {...t, isDone: isDone}: t))
-    setTasks({
-      ...tasks, [todolistId]: tasks[todolistId].map(checked => checked.id === idTask ? {
-        ...checked, isDone: isDone
-      } : checked)
-    })
-  }
+   }
+   const taskChecked = (todolistId: string, idTask: string, isDone: boolean) => {
+      // setTasks(tasks.map( t => t.id === idTask ? {...t, isDone: isDone}: t))
+      setTasks({
+         ...tasks, [todolistId]: tasks[todolistId].map(checked => checked.id === idTask ? {
+            ...checked, isDone: isDone
+         } : checked)
+      })
+   }
 
-  const taskTitleChange = (todolistId: string, idTask: string, title: string) => {
-    setTasks({
-      ...tasks, [todolistId]: tasks[todolistId].map(checked => checked.id === idTask ? {
-        ...checked, title: title
-      } : checked)
-    })
-  }
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  return (
-    // отрисовка тудулистов
-    <div className="app">
-
-      <AppBar position='static'>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color='inherit'
-            aria-label="menu"
-            sx={{mr: 2}}
-            onClick={handleClick}
-          >
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
-              }}
-            />
-
-          </IconButton>
+   const taskTitleChange = (todolistId: string, idTask: string, title: string) => {
+      setTasks({
+         ...tasks, [todolistId]: tasks[todolistId].map(checked => checked.id === idTask ? {
+            ...checked, title: title
+         } : checked)
+      })
+   }
 
 
-          <Typography variant='h5'>
-            Todolist
-          </Typography>
-          <Button color='inherit'>Login</Button>
-        </Toolbar>
-      </AppBar>
-      <AddItemForm addItem={addTodolist}/>
+   return (
+     // отрисовка тудулистов
+     <div className="app">
+        <Header/>
+        <Container fixed >
+           <Grid container style={{padding: '10px'}} >
+              <AddItemForm addItem={addTodolist}/>
+           </Grid>
+           <Grid container spacing={4}>
+              {
+                 todolist.map((mapTodolist) => {
+
+                    let tasksForTodolist = tasks[mapTodolist.id]
+
+                    if (mapTodolist.filter === 'Active') {
+                       tasksForTodolist = tasks[mapTodolist.id].filter(t => !t.isDone)
+                    }
+
+                    if (mapTodolist.filter === 'Completed') {
+                       tasksForTodolist = tasks[mapTodolist.id].filter(t => t.isDone)
+                    }
+
+                    return (
+                      <Grid item>
+                         <Paper style={{padding: '10px'}} elevation={5}>
+                            <Todolist
+                              key={mapTodolist.id}
+                              todolistId={mapTodolist.id}
+                              title={mapTodolist.title}
+                              tasks={tasksForTodolist}
+                              deleteTask={deleteTask}
+                              changeFilter={changeFilter}
+                              addTask={addTask}
+                              taskChecked={taskChecked}
+                              filter={mapTodolist.filter}
+                              taskTitleChange={taskTitleChange}
+                              todolistTitleChange={todolistTitleChange}
+                              deleteTodolist={deleteTodolist}
+                            />
+                         </Paper>
+                      </Grid>
+                    )
+                 })
+              }
+           </Grid>
+        </Container>
 
 
-      {todolist.map((mapTodolist) => {
-
-        let tasksForTodolist = tasks[mapTodolist.id]
-
-        if (mapTodolist.filter === 'Active') {
-          tasksForTodolist = tasks[mapTodolist.id].filter(t => !t.isDone)
-        }
-
-        if (mapTodolist.filter === 'Completed') {
-          tasksForTodolist = tasks[mapTodolist.id].filter(t => t.isDone)
-        }
-
-        return (
-          <Todolist
-            key={mapTodolist.id}
-            todolistId={mapTodolist.id}
-            title={mapTodolist.title}
-            tasks={tasksForTodolist}
-            deleteTask={deleteTask}
-            changeFilter={changeFilter}
-            addTask={addTask}
-            taskChecked={taskChecked}
-            filter={mapTodolist.filter}
-            taskTitleChange={taskTitleChange}
-            todolistTitleChange={todolistTitleChange}
-            deleteTodolist={deleteTodolist}
-          />
-        )
-      })}
-
-
-    </div>
-  );
+     </div>
+   );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {AppBar, Button, Menu, Toolbar, Typography} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import {AddItemForm} from "./AddItemForm";
+
+export const Header = () => {
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+      <>
+        <AppBar position='static'>
+          <Toolbar>
+            <IconButton
+                edge="start"
+                color='inherit'
+                aria-label="menu"
+                onClick={handleClick}
+            >
+              <MenuIcon/>
+              <Menu open={open} />
+            </IconButton>
+            <Typography variant='h5'>
+              Todolist
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar>
+
+      </>
+  );
+};
+
+export default Header;
